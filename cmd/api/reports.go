98744381cfaeaac18ab4669f43d4ce9d8a0fbb64
Teryn D. Guzman <2022156098@ub.edu.bz>
2026-08-24 21:53:20 -0600

    initial commit

---
+++ cmd/api/reports.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/lewisdalwin/gatekeeper/internal/data"
+	"github.com/lewisdalwin/gatekeeper/internal/validator"
+)
+
+func (app *application) createReportHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		ConsumerID string    `json:"consumer_id"`
+		From       time.Time `json:"from"`
+		To         time.Time `json:"to"`
+	}
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	v.Check(input.ConsumerID != "", "consumer_id", "must be provided")
+	v.Check(!input.From.IsZero(), "from", "must be provided")
+	v.Check(!input.To.IsZero(), "to", "must be provided")
+	v.Check(input.From.Before(input.To), "from", "must be earlier than to")
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	job := &data.Job{
+		ConsumerID: input.ConsumerID,
+		JobType:    "consumer_activity_report",
+		Payload:    data.ReportPayload{From: input.From, To: input.To},
+	}
+	if err := app.models.Jobs.Insert(job); err != nil {
+		if errors.Is(err, data.ErrRecordNotFound) {
+			app.notFoundResponse(w, r)
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	statusURL := fmt.Sprintf("/v1/jobs/%s", job.PublicID)
+	headers := make(http.Header)
+	headers.Set("Location", statusURL)
+	response := envelope{"job_id": job.PublicID, "status": job.Status, "status_url": statusURL}
+	if err := app.writeJSON(w, http.StatusAccepted, response, headers); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) getJobHandler(w http.ResponseWriter, r *http.Request) {
+	job, err := app.models.Jobs.GetByPublicID(r.PathValue("id"))
+	if err != nil {
+		if errors.Is(err, data.ErrRecordNotFound) {
+			app.notFoundResponse(w, r)
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	if err := app.writeJSON(w, http.StatusOK, envelope{"job": job}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
